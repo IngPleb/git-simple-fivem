fe8bad6d247efea6632ffeffc56bab111da5750c
Matěj Vnouček <matej.vnoucek@gmail.com>
2024-11-05 22:21:58 +0100

    chore: Update version to 1.2.0 and set server_only to 'yes'

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
+server_only 'yes' 
 
 author 'IngPleb'
 description 'A simple resource to pull Git changes with rebase'
-version '1.0.0'
+version '1.2.0'
 
 server_script 'server.lua'
