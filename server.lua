cd094e729a970f85b02c8600bfd4f768f46214b4
Matěj Vnouček <matej.vnoucek@gmail.com>
2024-11-07 20:50:28 +0100

    chore: Refactor server.lua

--- server.lua
+++ server.lua
@@ -1,14 +1,16 @@
 RegisterCommand('gitsimple', function(source, args, rawCommand)
-    if source == 0 then  -- Only allow execution from the console, not in-game
-        local gitCommand = GitCommand:new()
-        if args[1] == 'pull' then
-            gitCommand:pull({args[2]})
-        elseif args[1] == 'push' then
-            gitCommand:push()
-        else
-            print("Usage: gitsimple pull [--rebase]|push")
-        end
-    else
+    -- Check console
+    if source ~= 0 then
         print("This command can only be run from the server console.")
+        return
+    end
+
+    local gitCommand = GitCommand:new()
+    if args[1] == 'pull' then
+        gitCommand:pull({ args[2] })
+    elseif args[1] == 'push' then
+        gitCommand:push()
+    else
+        print("Usage: gitsimple pull [--rebase]|push")
     end
 end)
