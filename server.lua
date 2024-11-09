c3661152ddd4f7cc3a89eab689e5bb75dc4eb211
Matěj Vnouček <matej.vnoucek@gmail.com>
2024-11-09 11:47:44 +0100

    fix: Move command execution to a separate thread for performance

--- server.lua
+++ server.lua
@@ -1,18 +1,20 @@
 for _, name in ipairs(Config.GroupCommandNames) do
     RegisterCommand(name, function(source, args, rawCommand)
-        -- Check console
-        if source ~= 0 then
-            print("This command can only be run from the server console.")
-            return
-        end
+        CreateThread(function()
+            -- Check console
+            if source ~= 0 then
+                print("This command can only be run from the server console.")
+                return
+            end
 
-        local gitCommand = GitCommand:new()
-        if args[1] == 'pull' then
-            gitCommand:pull({ args[2] })
-        elseif args[1] == 'push' then
-            gitCommand:push()
-        else
-            print("Usage: " + name + " pull [--rebase]|push")
-        end
+            local gitCommand = GitCommand:new()
+            if args[1] == 'pull' then
+                gitCommand:pull({ args[2] })
+            elseif args[1] == 'push' then
+                gitCommand:push()
+            else
+                print("Usage: " + name + " pull [--rebase]|push")
+            end
+        end)
     end)
 end
